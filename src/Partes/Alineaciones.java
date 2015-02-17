7eaf24d1d80ddd5db567d3879f274763752f33ad
Unai <unai28850@gmail.com>
2015-02-17 20:15:50 +0100

    Update Alineaciones.java

--- src/Partes/Alineaciones.java
+++ src/Partes/Alineaciones.java
@@ -1,3 +1,4 @@
+
 package Partes;
 
 import java.io.BufferedReader;
@@ -6,20 +7,24 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.ObjectInputStream.GetField;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Alineaciones {
+	
 	
 	static ArrayList<JugadorSeleccion> delanteros= new ArrayList<JugadorSeleccion>();
 	static ArrayList<JugadorSeleccion> centrocampista= new ArrayList<JugadorSeleccion>();
 	static ArrayList<JugadorSeleccion> defensas= new ArrayList<JugadorSeleccion>();
 	static ArrayList<JugadorSeleccion> porteros= new ArrayList<JugadorSeleccion>();
+	static HashMap<Integer, JugadorSeleccion> alineacion = new HashMap<Integer, JugadorSeleccion>();
 
 	public static void main(String[] args) {
 		 
 	try {
 		 	String linea1;
-			File miArchivo = new File("C:/archivosJava/jugadores.txt");
+			File miArchivo = new File("src/jugadores.txt");
 			FileReader lector = new FileReader(miArchivo);
 			BufferedReader br = new BufferedReader(lector);
 			JugadorSeleccion jugador = null;
@@ -30,13 +35,13 @@ public class Alineaciones {
 			 	
 			while (( linea1 = br.readLine())!=null){
 					if( linea1.startsWith("-")){
-//					System.out.println(linea1);
+					System.out.println(linea1);
 					arrayJugador=linea1.substring(1).split(",");
 					
 					jugador = new JugadorSeleccion(arrayJugador); 
 					
 //					System.out.println(jugador.toString());
-					
+//					
 						if( jugador.getPosicion().equals( "delantero")){
 		
 							delanteros.add(jugador);
@@ -99,34 +104,110 @@ public class Alineaciones {
 			
 			//DEFENSAS
 			
-			System.out.println("Elija los defensas:\n ");
+			System.out.println("Elija los defensas, por su dorsal:\n ");
 			
 			for (int i=0; i<lineaDefensiva; i++){//repito la eleccion tantas veces como numero de defensas puso
 				
 				for (int j = 0; j< defensas.size();j++){
 					
-					System.out.println( defensas.get(j).getDorsal());
+					System.out.println( "DORSAL = " + defensas.get(j).getDorsal() + " NOMBRE = " + defensas.get(j).getNombre() + " POSICION = "  + defensas.get(j).getPosicion()+ " VECES INTERNACIONAL = " + defensas.get(j).getInternacionalidad());
 				
 				}
-					String defensaElejido = lector1.next();
-					System.out.println(defensaElejido);
+					String jugadorElejido = lector1.next();
 					
-					defensas.remove(jugador.getDorsal().equals(defensaElejido));
-					
-					
+//					System.out.println(defensaElejido);
+					 for (int k = 0; k< defensas.size();k++){
+						 if (defensas.get(k).getDorsal().equals(jugadorElejido)){
+							 alineacion.put(Integer.parseInt(defensas.get(k).getDorsal()), defensas.get(k));
+							 defensas.remove(k);
+//							 k = defensas.size()+1;
+						 }
+						 
+					 }
 			}
+			
 			
 			// CENTROCAMPISTAS
 			
-			System.out.println("Elija un centrocampista:\n ");
+			System.out.println("Elija los Centrocampistas, por su dorsal:\n ");
 			
-			for (int i = 0; i< centrocampista.size();i++){
+			for (int i=0; i<lineaCentral; i++){//repito la eleccion tantas veces como numero de defensas puso
 				
-				System.out.println( centrocampista.get(i).getNombre());
+				for (int j = 0; j< centrocampista.size();j++){
+					
+					System.out.println( "DORSAL = " + centrocampista.get(j).getDorsal() + " NOMBRE = " + centrocampista.get(j).getNombre() + " POSICION = "  + centrocampista.get(j).getPosicion()+ " VECES INTERNACIONAL = " + centrocampista.get(j).getInternacionalidad());
 				
-				
+				}
+					String jugadorElejido = lector1.next();
+					
+
+					 for (int k = 0; k< centrocampista.size();k++){
+						 if (centrocampista.get(k).getDorsal().equals(jugadorElejido)){
+							 alineacion.put(Integer.parseInt(centrocampista.get(k).getDorsal()), centrocampista.get(k));
+							 centrocampista.remove(k);
+						 }
+					 }
 			}
-			br.close();
+			
+			// DELANTEROS
+			
+						System.out.println("Elija los Delanteros, por su dorsal:\n ");
+						
+						for (int i=0; i<lineaDelantera; i++){//repito la eleccion tantas veces como numero de defensas puso
+							
+							for (int j = 0; j< delanteros.size();j++){
+								
+								System.out.println( "DORSAL = " + delanteros.get(j).getDorsal() + " NOMBRE = " + delanteros.get(j).getNombre() + " POSICION = "  + delanteros.get(j).getPosicion()+ " VECES INTERNACIONAL = " + delanteros.get(j).getInternacionalidad());
+							
+							}
+								String jugadorElejido = lector1.next();
+								
+
+								 for (int k = 0; k< delanteros.size();k++){
+									 if (delanteros.get(k).getDorsal().equals(jugadorElejido)){
+										 alineacion.put(Integer.parseInt(delanteros.get(k).getDorsal()), delanteros.get(k));
+										 delanteros.remove(k);
+									 }
+								 }
+						}
+						
+						//PORTEROS
+						
+						System.out.println("Elija los Porteros, por su dorsal:\n ");
+						
+						for (int i=0; i<1; i++){//repito la eleccion tantas veces como numero de defensas puso
+							
+							for (int j = 0; j< delanteros.size();j++){
+								
+								System.out.println( "DORSAL = " + porteros.get(j).getDorsal() + " NOMBRE = " + porteros.get(j).getNombre() + " POSICION = "  + porteros.get(j).getPosicion()+ " VECES INTERNACIONAL = " + porteros.get(j).getInternacionalidad());
+							
+							}
+								String jugadorElejido = lector1.next();
+								
+
+								 for (int k = 0; k< porteros.size();k++){
+									 if (porteros.get(k).getDorsal().equals(jugadorElejido)){
+										 alineacion.put(Integer.parseInt(porteros.get(k).getDorsal()), porteros.get(k));
+										 porteros.remove(k);
+									 }
+								 }
+						}
+						
+						//tengo que mostrar aqui el has de mi alineacion, aqui creo un array con los dorsales que metio y recupero el valor del hueco del array(dorsal) y muestro del has el jugador ocon sis valores
+//						System.out.println(alineacion.get());
+				//ESTO NO LO CONTROLO
+						Iterator it=  alineacion.keySet().iterator();// con esto establezcon este objeto iterator hace que avanzae de clave en clave es una coleccion que guarda las claves
+						
+						String clave;
+//						int valor;
+						
+						while(it.hasNext()){
+							clave= (String) it.next(); //devuelve la clave
+//							valor = alineacion.get(clave);//aqui el contenido se lo asigno a valor
+							System.out.println(clave+"="+ alineacion.get(clave));//aqui muestro la clave con su valor
+						}//si quisiese cambiar una 
+						
+						br.close();
 			
 		}
 				catch (IOException e) {
@@ -135,16 +216,3 @@ public class Alineaciones {
 
 	}
 }
-
-
-
-////DOrsal = 1
-//		Nombre= iker
-//		equipo = real
-//		posicion= porte
-//		internacionalidades= 23
-
-//for(int i = 0; i<defensas.size(); i++){
-//System.out.println(jugador.getDorsal(i));//con el for recorre las posiciones y las muestra.
-//}
-//
